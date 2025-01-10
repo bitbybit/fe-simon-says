de99212b337a28ed95c9956a0f49fdc1341b40b8
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-10 23:11:21 +0100

    feat: button class

---
+++ simon-says/service/ui/Button.js
@@ -0,0 +1,45 @@
+/**
+ * @typedef {{
+ *   title: string
+ *   onClick?: (e: Event) => {}
+ * }} ButtonProps
+ */
+
+export class Button {
+  /**
+   * @type {HTMLButtonElement}
+   */
+  $element
+
+  /**
+   * @param {ButtonProps} props
+   */
+  constructor({ title, onClick = () => {} }) {
+    this.$element = document.createElement('button')
+
+    this.$element.classList.add('btn')
+    this.$element.classList.add('btn-primary')
+
+    this.$element.innerText = title
+
+    this.$element.addEventListener('click', onClick)
+  }
+
+  disable() {
+    this.$element.setAttribute('disabled', 'disabled')
+  }
+
+  enable() {
+    this.$element.removeAttribute('disabled')
+  }
+
+  highlightOn() {
+    this.$element.classList.remove('btn-primary')
+    this.$element.classList.add('btn-info')
+  }
+
+  highlightOff() {
+    this.$element.classList.remove('btn-info')
+    this.$element.classList.add('btn-primary')
+  }
+}
